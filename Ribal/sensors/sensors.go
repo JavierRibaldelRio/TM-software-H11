fa3efe6bea39affa0c11bb2dca03fa20b1906e78
Javier Ribal del Río <javierribal@gmail.com>
2025-09-18 21:22:52 +0200

    feat: sensors.go stores the generator of data

--- Ribal/sensors/sensors.go
+++ Ribal/sensors/sensors.go
@@ -1,36 +1,66 @@
 package sensors
 
-import(
-
+import (
+	"context"
 	"math/rand"
 	"time"
 )
 
 type Sensor struct {
-    Name     	string
-    Unit     	string
-    Period   	time.Duration
-	Avg			float64	
-	Sdev		float64
+	Name   string
+	Number int64
+	Unit   string
+	Period time.Duration
+	Avg    float64
+	Sdev   float64
 }
 
-
-type Data struct{
-	Measure 		float64
-	Unit		string
-	Timestamp	time.Time
+type Data struct {
+	Measure   float64
+	Unit      string
+	Timestamp time.Time
+	Name      string
+	Number    int64
 }
 
-// It executes when the package is loaded
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
-func (s Sensor) Read () Data {
+// Using the properties of a Sensor generates following a normal distribution values
+func (s Sensor) Read() Data {
 	v := s.Avg + s.Sdev*rand.NormFloat64() // N(Avg, Sdev)
 	return Data{
 		Timestamp: time.Now(),
 		Measure:   v,
+		Name:      s.Name,
+		Number:    s.Number,
 		Unit:      s.Unit,
 	}
+}
+
+// StartMeasuring starts a goroutine that periodically sends readings to out.
+func (s Sensor) StartMeasuring(ctx context.Context, out chan<- Data) {
+
+	// Starts the gororutine, uses anonymous function
+	go func() {
+
+		ticker := time.NewTicker(s.Period) // Creates a new ticker for each period
+		defer ticker.Stop()                // Ensures that the ticker is properly removed
+
+		// Un
+		for {
+
+			// Wait until a case ocurrs
+			select {
+
+			// Tick
+			case <-ticker.C:
+				out <- s.Read()
+
+			// Stop app
+			case <-ctx.Done():
+				return
+
+			}
+
+		}
+
+	}()
 }
