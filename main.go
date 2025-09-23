b57562e1fc8f57afc3d94c7734ffe63bc951d352
Javier Ribal del Río <javierribal@gmail.com>
2025-09-23 23:22:33 +0200

    refactor: simplify main logic

--- main.go
+++ main.go
@@ -25,45 +25,16 @@ func main() {
 	// Starts all the sensors
 	StartSensors(ctx, readings)
 
-	// Create file
-	filename := fmt.Sprintf("output_%s.csv", time.Now().Format("20060102_150405"))
-
-	f, err := os.Create(filename)
-	if err != nil {
-		panic(err)
-	}
-
-	// Create writer
-	writer := csv.NewWriter(f)
-	defer writer.Flush()
-
-	// write header in log
-	writer.Write([]string{"timestamp", "magnitude", "avg", "min", "max"})
+	// CSV writer
+	writer, closeFile := SetUpCSVWriter()
+	defer closeFile()
 
 	// For each second
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
-	// Forever
-	for {
-
-		select {
-
-		// Adds the reading to de batch
-		case d := <-readings:
-			data[d.Name] = append(data[d.Name], d.Measure)
-
-		// Each seconds writes the log
-		case t := <-ticker.C:
-			ParseDataWriteLog(data, t, writer)
-
-		// Stop app
-		case <-ctx.Done():
-			return
-
-		}
-
-	}
+	// Register the output of the sensors
+	runProcessingLoop(ctx, readings, data, ticker, writer)
 
 }
 
@@ -119,5 +90,54 @@ func ParseDataWriteLog(data map[string][]float64, t time.Time, writer *csv.Write
 
 		// Remove content
 		data[magnitude] = data[magnitude][:0]
+	}
+}
+
+// setCSVWriter configures and return the csv file writer and a function
+// to close the writer
+func SetUpCSVWriter() (*csv.Writer, func()) {
+
+	// Create file
+	filename := fmt.Sprintf("output_%s.csv", time.Now().Format("20060102_150405"))
+
+	f, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create writer
+	writer := csv.NewWriter(f)
+
+	// write header in log
+	writer.Write([]string{"timestamp", "magnitude", "avg", "min", "max"})
+
+	// Retunrs thew writer and a function to close it
+	return writer, func() {
+		writer.Flush()
+		f.Close()
+	}
+}
+
+// runProcessingLoops recives the output of the sensors, stores it and obtains the main stats
+func runProcessingLoop(ctx context.Context, readings chan sensors.Data, data map[string][]float64, ticker *time.Ticker, writer *csv.Writer) {
+	// Forever
+	for {
+
+		select {
+
+		// Adds the reading to de batch
+		case d := <-readings:
+			data[d.Name] = append(data[d.Name], d.Measure)
+
+		// Each seconds writes the log
+		case t := <-ticker.C:
+			ParseDataWriteLog(data, t, writer)
+
+		// Stop app
+		case <-ctx.Done():
+			return
+
+		}
+
 	}
 }
